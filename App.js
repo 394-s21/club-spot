f5307a9119384485658b4d719d0397b863c151d4
peiyj <yingjianpei2022@u.northwestern.edu>
2021-05-05 15:00:47 -0700

    set up club home page

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import clubHomePage from './screens/clubHomePage';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+// import DashboardStack froma './navigations/dashboardStack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="clubHomePage" 
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen
+          name="clubHomePage"
+          component={clubHomePage}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>)}
