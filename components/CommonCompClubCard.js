a2d2ee30dd4e18e4d6928ba77338d359b3c05755
Tony Bayvas <tbayvas@outlook.com>
2021-05-06 15:47:54 -0500

    got left styling to work

--- components/CommonCompClubCard.js
+++ components/CommonCompClubCard.js
@@ -3,15 +3,14 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import { Card } from 'react-native-paper';
 
 const CommonCompClubCard = () => {
-    const LeftContent = () => <View style={styles.imageContainer}>
-                                <Image style={styles.clubImage} source={{uri: 'https://www.ieee.org/content/dam/ieee-org/ieee/web/org/about/whatis/71858.gif'}}/>
-                              </View>
+    const LeftContent = () => <Image style={styles.clubImage} source={{uri: 'https://www.ieee.org/content/dam/ieee-org/ieee/web/org/about/whatis/71858.gif'}}/>
   
     return(
         <View style={styles.container}>
-            <Card style={{backgroundColor: 'green', flexDirection: 'row', height: 300}}>
+            <Card style={{backgroundColor: 'white'}}>
                 <Card.Title style={styles.cardContent}
-                left={() => <Image style={styles.clubImage} source={{uri: 'https://www.ieee.org/content/dam/ieee-org/ieee/web/org/about/whatis/71858.gif'}}/>}
+                left={LeftContent}
+                leftStyle={styles.imageContainer}
                 title="Club Name" 
                 subtitle="Completed: 1/6 w style={styles.container}>
                 <Card style={{backgroundColor: 'green', flexDirection: 'row'}}>
@@ -31,18 +30,14 @@ const CommonCompClubCard = () => {
     },
     clubImage: {
       borderRadius: 25,
-      // borderWidth: 2,
+      borderWidth: 2,
       width: 50,
       height: 50
     },
     imageContainer: {
       paddingHorizontal: 10,
-      borderWidth: 2,
-      //marginHorizontal: 20,
-      width: 80
+      //borderWidth: 2,
+      width: 70
     },
-    cardContent: {
-      flexDirection: 'row'
-    }
   });
   export default CommonCompClubCard;
