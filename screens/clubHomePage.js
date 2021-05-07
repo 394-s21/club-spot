9fc31a49cbe71c2a704920cc1770b490d8081d1e
Caroline Lobel <57736173+carolinelobel@users.noreply.github.com>
2021-05-07 11:14:50 -0500

    search functionality and component

--- screens/clubHomePage.js
+++ screens/clubHomePage.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView} from 'react-native';
+import { StyleSheet, View, SafeAreaView, ScrollView} from 'react-native';
 import CommonCompClubCard from '../components/CommonCompClubCard';
 import { firebase }  from '../firebase';
+import { Provider, TextInput, RadioButton,Text, Subheading,Card, Button,Paragraph, Dialog, Portal } from 'react-native-paper';
+import filter from 'lodash.filter';
 
 class clubHomePage extends Component{
   constructor(props) {
     super(props);
 
     this.state = {
-      clubs: []
+      clubs: [],
+      all_clubs: [],
+      query: ""
     };
+    this.handleSearch = this.handleSearch.bind(this)
+    this.contains = this.contains.bind(this)
   }
 
   componentDidMount() {
@@ -21,16 +27,49 @@ class clubHomePage extends Component{
         snapshot.forEach(function (childSnapshot) {
           clubArray.push(childSnapshot.toJSON());
         });
-      this.setState({clubs: clubArray});
+      this.setState({clubs: clubArray,
+                    all_clubs: clubArray});
       }
     })
   };
+  contains = (club,query) => {
+    if (club.clubName.toLowerCase().includes(query) || club.category.toLowerCase().includes(query)) {
+      console.log('club found')
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  handleSearch(queryText) {
+    if (queryText === "") {
+      this.setState({clubs: this.state.all_clubs})
+    }
+    else {
+      console.log('query: ',queryText)
+      const formattedQuery = queryText.toLowerCase();
+      this.setState({query: formattedQuery})
+      const all_clubs = this.state.clubs
+      const filteredClubs = filter(all_clubs, club => {
+        return this.contains(club,formattedQuery);
+      })
+      console.log('filtered clubs: ',filteredClubs)
+      this.setState({clubs: filteredClubs})
+    }
+  }
+  
 
   render(){
     return(
       <SafeAreaView>
         <ScrollView>
           <View>
+              <TextInput label="Search" 
+                        value = {this.state.query} 
+                        type="outlined" 
+                        style = {styles.field}
+                        onChangeText={queryText => this.handleSearch(queryText)} />
            <CommonCompClubCard clubName="Club Name" clubDesc="This is a club about friendship and trust here at Northwestern. To learn more"/>
            <View>
            {this.state.clubs.map(club => <CommonCompClubCard clubName={club.clubName} clubDesc={club.description}/>)}
@@ -41,4 +80,13 @@ class clubHomePage extends Component{
     ) 
   }
 }
+
+const styles = StyleSheet.create({
+  field: {
+    height: 55,
+    margin: 12,
+    backgroundColor: 'white',
+  },
+})
+
 export default clubHomePage;
