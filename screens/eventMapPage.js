f92f191e4fc2458e3965090e5287162b66e30ea0
peiyj <yingjianpei2022@u.northwestern.edu>
2021-05-07 17:16:06 -0700

    set initial location to northwestern

--- screens/eventMapPage.js
+++ screens/eventMapPage.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 class eventMapPage extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      initLocation : {
+        latitude: 42.055984,
+        longitude: -87.675171,
+      }
+    }
+  }
+
   render() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );}
+    return (
+        <MapView
+            style={{ flex: 1 }}
+            region={{
+                latitude: this.state.initLocation.latitude,
+                longitude: this.state.initLocation.longitude,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
+            }}
+        />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
