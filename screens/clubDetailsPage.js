ff68cc67b58654c8211b13c907b93b46a471bc48
Lukas <lukasbill@163.com>
2021-05-11 19:57:51 +0800

    Simple stying

--- screens/clubDetailsPage.js
+++ screens/clubDetailsPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import clubHomePage from './clubHomePage';
-import { StyleSheet, View, Text, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, Image, SafeAreaView, ScrollView } from 'react-native';
 
 
 
@@ -12,19 +12,54 @@ class clubDetailsPage extends Component{
             clubDesc : this.props.route.params.clubDesc
         }
     }
-    
+
 
     render(){
         return(
             <SafeAreaView>
-                <Text>{this.state.clubName}</Text>
-                <Text>{this.state.clubDesc}</Text>
+                <ScrollView>
+                    <Text style = {styles.title}>{this.state.clubName}
+                    {'\n'}
+                    </Text>
+                    <Text style = {styles.clubDescription}>{this.state.clubDesc}</Text>
+                </ScrollView>
             </SafeAreaView>
         )
     }
 
 }
 
-
+const styles = StyleSheet.create({
+    title: {
+      fontSize: 22,
+      paddingTop: 10
+    },
+    container: {
+      flex: 1,
+      padding: 10,
+      borderRadius: 40
+    },
+    clubImage: {
+      borderRadius: 25,
+      borderWidth: 2,
+      width: 50,
+      height: 50,
+      marginTop: 15,
+      alignContent: 'center',
+      justifyContent: 'center'
+    },
+    imageContainer: {
+      paddingHorizontal: 0,
+      //borderWidth: 2,
+      width: 60
+    },
+    clubDescription: {
+      fontSize: 14,
+      
+    },
+    card: {
+      height: 100
+    },
+  });
 
 export default clubDetailsPage;
