8393e0a24ed47d0a062136afac3a8cc28a0cac93
Caroline Lobel <57736173+carolinelobel@users.noreply.github.com>
2021-05-12 08:06:17 -0400

    changes

--- screens/clubHomePage.js
+++ screens/clubHomePage.js
@@ -65,12 +65,41 @@ class clubHomePage extends Component{
       <SafeAreaView>
         <ScrollView>
           <View>
+<<<<<<< Updated upstream
               <TextInput label="Search" 
                         value = {this.state.query} 
                         type="outlined" 
                         style = {styles.field}
                         onChangeText={queryText => this.handleSearch(queryText)} />
            <CommonCompClubCard clubName="Club Name" clubDesc="This is a club about friendship and trust here at Northwestern. To learn more"/>
+=======
+            <View>
+            
+            <TextInput label="Search"
+                       value = {this.state.query} 
+                       type = "flat" 
+                       style = {styles.field}
+                       placeholder = "Search for group"
+                       onChangeText={queryText => this.handleSearch(queryText)} 
+            />
+            <DropDown
+                style={styles.dropdown}
+                label={'Filter category'}
+                placeholder="Category"
+                mode={'outlined'}
+                value={this.state.clubCat}
+                setValue={(item) => this.filter(item) }
+                list={this.state.clubCategories}
+                visible={this.state.showDropDown}
+                showDropDown={() =>  this.setState({showDropDown: true})}
+                onDismiss={() =>  this.setState({showDropDown: false})}
+                inputProps={{
+                right:  <TextInput.Icon  name={'menu-down'}  />,
+                }}
+                />
+                
+           </View>
+>>>>>>> Stashed changes
            <View>
            {this.state.clubs.map(club => <CommonCompClubCard clubName={club.clubName} clubDesc={club.description}/>)}
            </View>
@@ -82,11 +111,39 @@ class clubHomePage extends Component{
 }
 
 const styles = StyleSheet.create({
+  dropdown: {
+    padding: 20
+  },
   field: {
     height: 55,
+<<<<<<< Updated upstream
+=======
+    flex: 1,
+>>>>>>> Stashed changes
     margin: 12,
-    backgroundColor: 'white',
+    backgroundColor: 'white'
   },
+<<<<<<< Updated upstream
+=======
+  row: {
+    margin: 4,
+    flexDirection: "row"
+  },
+  container: {
+
+    flex:  1,
+      
+    marginHorizontal:  20,
+      
+    justifyContent:  'center',
+      
+  },
+
+  searchbar:{
+    margin:10,
+    backgroundColor: 'white',
+  }
+>>>>>>> Stashed changes
 })
 
 export default clubHomePage;
