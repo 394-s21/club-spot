5e0fc44e75223df3746ff95f4ebcc38026c12eec
peiyj <yingjianpei2022@u.northwestern.edu>
2021-05-13 17:00:36 -0700

    change default screen to login

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName="loginPage" 
+        initialRouteName="homeTab" // TODO: change back to login in the final stage
         screenOptions={{
           headerShown: false
         }}>
