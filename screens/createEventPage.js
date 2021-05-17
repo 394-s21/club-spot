eada2535a284d4d56eb47bdca10efa45e84c8831
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-05-17 12:29:03 -0500

    date picker

--- screens/createEventPage.js
+++ screens/createEventPage.js
@@ -3,6 +3,8 @@ import { Provider, TextInput, RadioButton,Text, Subheading,Card, Button,Paragrap
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import Geocoder from 'react-native-geocoding'; 
 import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 class eventMapPage extends Component{
   constructor(props) {
@@ -14,20 +16,29 @@ class eventMapPage extends Component{
       address: "",
       activity: "",
       date: "",
+      datePickerVisibility: false
     }
   }
 
   translateAddress = () => {
     console.log(`hello`)
   }
+
+  showDatePicker = () => {
+      this.setState({datePickerVisibility: true})
+  };
+
+  hideDatePicker = () => {
+    this.setState({datePickerVisibility: false})
+  };
+
+  handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    this.hideDatePicker();
+  };
+
   render(){
     return(
-      // <TextInput label='Club Name' 
-      //   value={this.state.clubName} 
-      //   type="outlined"
-        
-      //   style={styles.field}
-      //   onChangeText={text => this.setState({clubName:text})} />
 
       // <TextInput label='Activity' 
       //   value={this.state.activity} 
@@ -42,6 +53,24 @@ class eventMapPage extends Component{
         
       //   style={styles.field}
       //   onChangeText={text => this.setState({date:text})} /> 
+    <View style={styles.container}>
+        <View style={{height: "100%", width: "100%", backgroundColor: "grey"}}>
+        <TextInput label='Club Name' 
+         value={this.state.clubName} 
+         type="outlined"
+        
+         style={styles.field}
+         onChangeText={text => this.setState({clubName:text})} />
+      <View style={{flexDirection: "row"}}>
+         <TouchableOpacity style={{width: 100, height: 30, backgroundColor: "red"}} onPress={() => this.showDatePicker()} title="Date" />
+         <TouchableOpacity style={{width: 100, height: 30, backgroundColor: "green"}} onPress={() => this.showTimepicker} title="Time" />
+      </View>
+      <DateTimePickerModal
+        isVisible={this.state.datePickerVisibility}
+        mode="date"
+        onConfirm={(x) => this.handleConfirm(x)}
+        onCancel={(x) => this.hideDatePicker(x)}
+      />
         <GooglePlacesAutocomplete
           placeholder='Address'
           onPress={(data, details = null) => {
@@ -53,6 +82,8 @@ class eventMapPage extends Component{
             language: 'en',
           }}
         />
+        </View>
+    </View>
     )
   }
 }
