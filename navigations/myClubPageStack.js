9ce432e0908bf3e38d96797e79a8d106d3de07fc
peiyj <yingjianpei2022@u.northwestern.edu>
2021-05-21 21:45:40 -0700

    added basic chat

--- navigations/myClubPageStack.js
+++ navigations/myClubPageStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import clubAnnoucementPage from "../screens/clubAnnoucementPage";
 import myClubPage from "../screens/myClubPage";
+import groupChatPage from "../screens/groupChatPage";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,10 @@ export default function myClubPageStack(){
             <Stack.Screen
             name='Club Annoucement'
             component={clubAnnoucementPage}
+            />
+            <Stack.Screen
+            name='Chat'
+            component={groupChatPage}
             />
         </Stack.Navigator>
     )
