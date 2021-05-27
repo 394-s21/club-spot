a1f0ef8172bd51f0ea2b50caf246c0de8235d9dd
Tony Bayvas <tbayvas@outlook.com>
2021-05-27 16:30:02 -0500

    highlight when you select a tab

--- components/CommonCompGoogleSignIn.js
+++ components/CommonCompGoogleSignIn.js
@@ -31,7 +31,7 @@ export default SocialButton;
 const styles = StyleSheet.create({
   buttonContainer: {
     marginTop: 10,
-    width: '100%',
+    width: '93%',
     height: windowHeight / 15,
     padding: 10,
     flexDirection: 'row',
