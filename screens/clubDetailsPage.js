1e81fbd1570f742b3ee4395f0415ad2d90fa486e
Lukas <lukasbill@163.com>
2021-05-28 21:41:43 +0800

    Add image picker embedding

--- screens/clubDetailsPage.js
+++ screens/clubDetailsPage.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Image, SafeAreaView, ScrollView, Alert } from 'react-native';
+import React, { Component, useState, useEffect} from 'react';
+import { StyleSheet, View, Image, SafeAreaView, ScrollView, Alert, Platform } from 'react-native';
 import { Avatar,Provider, TextInput, RadioButton,Text, Subheading,Title, Card, Button,Paragraph, Dialog, Portal } from 'react-native-paper';
 
 import {firebase} from '../utils/firebase';
 import { Linking } from 'react-native'
 
+import * as ImagePicker from 'expo-image-picker';
 class clubDetailsPage extends Component{
     constructor(props){
         super(props);
@@ -20,8 +21,8 @@ class clubDetailsPage extends Component{
             edit: false,
             newCat: this.props.route.params.clubCategory,
             newDescription: this.props.route.params.clubDesc,
-            newName: this.props.route.params.clubName
-
+            newName: this.props.route.params.clubName,
+            image:'../assets/clubLogo.png'
         }
         this.handleEmailClick = this.handleEmailClick.bind(this)
         this.groupButton = this.groupButton.bind(this)
@@ -144,8 +145,32 @@ class clubDetailsPage extends Component{
          //           })
         //console.log('userInfo: ',this.state.userInfo)
         });
+        this.askPermission()
     }
+    pickImage = async()=>{
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes:ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect:[4,3],
+            quality:1,
+        });
+
+        console.log(result);
+        if (!result.cancelled){
+            setState({image:result.uri});
+        }
+        console.log(this.state.image)
+    };
+
     
+    askPermission = async()=>{
+        if(Platform.OS !== 'web'){
+            const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted'){
+                alert('Sorry, we need camera roll permission!');
+            }
+        }
+    }
 
     groupButton() {
         const mem = this.state.clubMember
@@ -251,6 +276,12 @@ class clubDetailsPage extends Component{
                         {this.state.clubName}
                             {'\n'}
                         </Text>
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                            <Image source={this.state.image} style={{ width: 200, height: 200 }} />
+                            <Button onPress={this.pickImage}>
+                                Pick an image from camera roll
+                            </Button>
+                        </View>
 
                         <Card style={styles.card}>
                             <Card.Content>
@@ -291,6 +322,12 @@ class clubDetailsPage extends Component{
                 <ScrollView>
                     <View style={styles.container}>
                     <Title style={styles.subheading}>{this.state.clubName}</Title>
+                    </View>
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Image source={this.state.image} style={{ width: 200, height: 200 }} />
+                        <Button onPress={this.pickImage}>
+                        Pick an image from camera roll
+                        </Button>
                     </View>
                     <Subheading>CATEGORY</Subheading>
                     <Card style={styles.card}>
